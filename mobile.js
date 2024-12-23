a7a88e4f3a10313216267bb0638a6110a4c24f60
Linus Gasser <linus.gasser@epfl.ch>
2024-12-23 23:42:49 +0100

    More fullscreen

--- mobile.js
+++ mobile.js
@@ -1,10 +1,34 @@
 var elem = document.documentElement;
 if (elem.requestFullscreen) {
-    elem.requestFullscreen();
+    elem.requestFullscreen()
+        .then(() => { })
+        .catch((err) => {
+            alert(
+                `An error occurred while trying to switch into fullscreen mode: ${err.message} (${err.name})`,
+            );
+        });
 } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
+    elem.mozRequestFullScreen()
+        .then(() => { })
+        .catch((err) => {
+            alert(
+                `An error occurred while trying to switch into fullscreen mode: ${err.message} (${err.name})`,
+            );
+        });
 } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-    elem.webkitRequestFullscreen();
+    elem.webkitRequestFullscreen()
+        .then(() => { })
+        .catch((err) => {
+            alert(
+                `An error occurred while trying to switch into fullscreen mode: ${err.message} (${err.name})`,
+            );
+        });
 } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
+    elem.msRequestFullscreen()
+        .then(() => { })
+        .catch((err) => {
+            alert(
+                `An error occurred while trying to switch into fullscreen mode: ${err.message} (${err.name})`,
+            );
+        });
 }
